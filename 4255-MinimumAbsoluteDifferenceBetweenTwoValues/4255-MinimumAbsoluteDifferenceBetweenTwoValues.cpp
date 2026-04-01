a8f6287dd29064e0b5a94d13da79858e4652fd14
mahinagasasidhar <mahinagasasidhar2006@gmail.com>
2026-04-01 19:23:20 +0530

    Create 4255-MinimumAbsoluteDifferenceBetweenTwoValues

---
+++ 4255-MinimumAbsoluteDifferenceBetweenTwoValues/4255-MinimumAbsoluteDifferenceBetweenTwoValues.cpp
@@ -0,0 +1,25 @@
+// Last updated: 01/04/2026, 19:23:19
+class Solution {
+public:
+    int minAbsoluteDifference(vector<int>& nums) {
+        int last1 = -1, last2 = -1;
+        int ans = INT_MAX;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == 1) {
+                last1 = i;
+                if (last2 != -1) {
+                    ans = min(ans, abs(last1 - last2));
+                }
+            }
+            else if (nums[i] == 2) {
+                last2 = i;
+                if (last1 != -1) {
+                    ans = min(ans, abs(last1 - last2));
+                }
+            }
+        }
+
+        return (ans == INT_MAX) ? -1 : ans;
+    }
+};
