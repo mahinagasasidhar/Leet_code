ff8fbe0b9a84c74e889a34ef62d158c63239b6f9
mahinagasasidhar <mahinagasasidhar2006@gmail.com>
2026-04-01 19:28:31 +0530

    Create 73-SetMatrixZeroes

---
+++ 73-SetMatrixZeroes/73-SetMatrixZeroes.cpp
@@ -0,0 +1,26 @@
+// Last updated: 01/04/2026, 19:28:30
+class Solution {
+public:
+ 
+    void setZeroes(vector<vector<int>>& matrix) {
+      int n = matrix.size();
+      int m = matrix[0].size(); 
+      vector<int>ar(n,0);
+      vector<int>ac(m,0);
+      for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+          if(matrix[i][j]==0){
+            ar[i]=1;
+            ac[j]=1;
+          }
+        }
+      }
+       for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(ar[i]==1 || ac[j]==1){
+                matrix[i][j]=0;
+            }
+        }
+       }
+      }
+};
