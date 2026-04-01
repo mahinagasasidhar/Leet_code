234c92cb1853d97523680061868b7893088fc07a
mahinagasasidhar <mahinagasasidhar2006@gmail.com>
2026-04-01 19:28:15 +0530

    Create 102-BinaryTreeLevelOrderTraversal

---
+++ 102-BinaryTreeLevelOrderTraversal/102-BinaryTreeLevelOrderTraversal.cpp
@@ -0,0 +1,26 @@
+// Last updated: 01/04/2026, 19:28:13
+
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+    vector<vector<int>>ans;
+    if(root==nullptr){
+        return ans;
+    }
+    queue<TreeNode*>q;
+    q.push(root);
+    while(!q.empty()){
+    int size = q.size();
+    vector<int>level;
+     for(int i=0;i<size;i++){
+        TreeNode* node = q.front();
+        q.pop();
+        if(node->left!=nullptr) q.push(node->left);
+         if(node->right!=nullptr) q.push(node->right);
+         level.push_back(node->val);
+     }
+     ans.push_back(level);
+    }
+     return  ans;  
+    }
+};
