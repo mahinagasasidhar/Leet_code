1adf19dc0262e31e7905e10c4b65b869313c31de
mahinagasasidhar <mahinagasasidhar2006@gmail.com>
2026-04-01 23:52:13 +0530

    Create 72-EditDistance

---
+++ 72-EditDistance/version/cpp/72-EditDistance_20260401_235212.cpp
@@ -0,0 +1,18 @@
+// Last updated: 01/04/2026, 23:52:12
+1class Solution {
+2public:
+3    int n,m;
+4    int rec(int i,int j,string& a,string& b,vector<vector<int>>& dp){
+5        if(j == m) return n - i;
+6        if(i == n) return m - j;
+7        if(dp[i][j] != -1) return dp[i][j];
+8        if(a[i] == b[j]) return dp[i][j] = rec(i+1,j+1,a,b,dp);
+9        return dp[i][j] = min({1+rec(i+1,j,a,b,dp),1+rec(i+1,j+1,a,b,dp),1+rec(i,j+1,a,b,dp)});
+10    }
+11    int minDistance(string word1, string word2) {
+12        n = word1.size();
+13        m = word2.size();
+14        vector<vector<int>> dp(n,vector<int>(m,-1));
+15        return rec(0,0,word1,word2,dp);
+16    }
+17};
